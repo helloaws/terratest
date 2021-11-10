7bafa2fceedb3ff0c4a5fa40f9ffd1c1b5d9cd0d
helloaws <woghks7065@naver.com>
2021-11-10 17:31:26 +0900

    step1

---
+++ 05_rt.tf
@@ -0,0 +1,12 @@
+resource "aws_route_table" "lee_rf" {
+  vpc_id = aws_vpc.hello-vpc.id
+ 
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.lee_ig.id
+  }
+  tags = {
+    "Name" = "lee-rt"
+  }
+  
+}
