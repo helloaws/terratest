5cbabe12472015d3f4b122465b48045d5c68b5b7
helloaws <woghks7065@naver.com>
2021-11-11 16:22:53 +0900

    step1

---
+++ 09_ngass.tf
@@ -0,0 +1,9 @@
+resource "aws_route_table_association" "lee_ngass_a" {
+  subnet_id = aws_subnet.lee_pria.id
+  route_table_id = aws_route_table.lee_ngrt.id
+}
+
+resource "aws_route_table_association" "lee_ngass_c" {
+  subnet_id = aws_subnet.lee_pric.id
+  route_table_id = aws_route_table.lee_ngrt.id
+}
