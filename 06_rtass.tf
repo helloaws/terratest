7cadab867f978ead8931769e287844c409b5e8d8
helloaws <woghks7065@naver.com>
2021-11-11 15:30:58 +0900

    step1

---
+++ 06_rtass.tf
@@ -0,0 +1,11 @@
+resource "aws_route_table_association" "lee_rtass_a" {
+  subnet_id = aws_subnet.lee_puba.id
+  route_table_id = aws_route_table.lee_rf.id
+
+}
+
+resource "aws_route_table_association" "lee_rtass_c" {
+  subnet_id = aws_subnet.lee_puba.id
+  route_table_id = aws_route_table.lee_rf.id
+
+}
