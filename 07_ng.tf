fc64af99c2f29b55148d59011014e2dfbf45fb79
helloaws <woghks7065@naver.com>
2021-11-11 18:18:46 +0900

    step2

--- 07_ng.tf
+++ 07_ng.tf
@@ -1,13 +1,16 @@
+//EIP 생성
 resource "aws_eip" "lee_eip_ng" {
   vpc = true
 }
 
+//nat gateway 생성
 resource "aws_nat_gateway" "lee_ng" {
-  allocation_id = aws_eip.lee_eip_ng.id
-  subnet_id = aws_subnet.lee_puba.id
+  allocation_id = aws_eip.lee_eip_ng.id //eip를 natgateway에 설정
+  subnet_id = aws_subnet.lee_puba.id //natgateway를 배치할 서브넷
   tags = {
     "Name" = "lee_ng"
   }
+  //nat gateway를 사용하기위해선 igw가 선행되어야하기때문에 설정
   depends_on = [
     aws_internet_gateway.lee_ig
   ]
