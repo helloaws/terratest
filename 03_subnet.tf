08216f624e85db0f0f7a62749bd6147fdb1c7b55
helloaws <woghks7065@naver.com>
2021-11-12 16:22:04 +0900

    step4

--- 03_subnet.tf
+++ 03_subnet.tf
@@ -8,6 +8,7 @@ resource "aws_subnet" "lee_puba" {
     "Name" = "lee-puba"
   }
 }
+
 // 가용영역 a의 private subnet
 resource "aws_subnet" "lee_pria" {
   vpc_id = aws_vpc.hello-vpc.id
@@ -17,6 +18,7 @@ resource "aws_subnet" "lee_pria" {
     "Name" = "lee-pria"
   }
 }
+
 //가용영역 c의 public subnet
 resource "aws_subnet" "lee_pubc" {
   vpc_id = aws_vpc.hello-vpc.id
@@ -26,6 +28,7 @@ resource "aws_subnet" "lee_pubc" {
     "Name" = "lee-pubc"
   }
 }
+
 // 가용영역 c의 private subnet
 resource "aws_subnet" "lee_pric" {
   vpc_id = aws_vpc.hello-vpc.id
@@ -33,5 +36,25 @@ resource "aws_subnet" "lee_pric" {
   availability_zone = "ap-northeast-2c"
   tags = {
     "Name" = "lee-pric"
+  }
+}
+
+//가용영역의 a의 private DB subnet
+resource "aws_subnet" "lee_priba" {
+  vpc_id = aws_vpc.hello-vpc.id
+  cidr_block = "10.0.4.0/24"
+  availability_zone = "ap-northeast-2a"
+  tags = {
+    "Name" = "lee-priba"
+  }
+}
+
+//가용영역의 c의 private DB subnet
+resource "aws_subnet" "lee_pribc" {
+  vpc_id = aws_vpc.hello-vpc.id
+  cidr_block = "10.0.5.0/24"
+  availability_zone = "ap-northeast-2c"
+  tags = {
+    "Name" = "lee-pribc"
   }
 }
