d3beecec40e9f040a4d343923a6beb185abba3b4
helloaws <woghks7065@naver.com>
2021-11-12 14:21:28 +0900

    step1

--- 15_albtgatt.tf
+++ 15_albtgatt.tf
@@ -1,8 +1,8 @@
-
+//부하분산 타겟그룹 대상지정
 resource "aws_lb_target_group_attachment" "lee_tgatt" {
   target_group_arn = aws_lb_target_group.lee_albtg.arn
   target_id = aws_instance.lee_weba.id
   port = 80
-  
+
 
 }
